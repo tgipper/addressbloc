9ff2cbb8a813b2c409399d42affe7ef4f30aab33
Trevor Gipper <trevor.gipper@gmail.com>
2017-07-19 13:31:23 -0700

    complete models-assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,7 +9,7 @@ class AddressBook
 
     def add_entry(name, phone_number, email)
         index = 0
-        entries.each do |entry|
+        @entries.each do |entry|
             if name < entry.name
                 break
             end
@@ -17,5 +17,17 @@ class AddressBook
         end
 
         entries.insert(index, Entry.new(name, phone_number, email))
+    end
+
+    def remove_entry(name, phone, email)
+        delete_entry = nil
+
+        @entries.each do |entry|
+            if name == entry.name && phone == entry.phone_number && email == entry.email
+                delete_entry = entry
+            end
+        end
+        p delete_entry
+        @entries.delete(delete_entry)
     end
 end
