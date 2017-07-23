b4e947954185dde197253752300020d367872b14
Trevor Gipper <trevor.gipper@gmail.com>
2017-07-23 11:46:34 -0700

    complete searching assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -61,4 +61,14 @@ class AddressBook
 
         return nil
     end
+
+    def iterative_search(name)
+        @entries.each do |entry|
+            if name == entry.name
+                return entry
+            end
+        end
+
+        return nil
+    end
 end
